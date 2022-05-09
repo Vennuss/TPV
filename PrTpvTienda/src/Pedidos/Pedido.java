7153f1ce71c98b28174b93fcdafcaef1f47d5fad
Vennuss <huchanserio@gmail.com>
2022-05-09 20:07:07 +0200

    Clase Pedidos

---
+++ PrTpvTienda/src/Pedidos/Pedido.java
@@ -0,0 +1,135 @@
+package Pedidos;
+
+import bd.bd;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author admin
+ */
+public class Pedido {
+    
+    private int id;
+    private boolean estadoPago;
+    private String fecha = null, formaPago, cliente;
+    
+    public Pedido(String _fecha, String _formaPago, boolean _estadoPago, String _cliente){
+        this.fecha = _fecha;
+        this.formaPago = _formaPago;
+        this.estadoPago = _estadoPago;
+        this.cliente = _cliente;
+    }
+    
+    public Pedido(String _formaPago, boolean _estadoPago, String _cliente){
+        this.formaPago = _formaPago;
+        this.estadoPago = _estadoPago;
+        this.cliente = _cliente;
+    }
+    
+    public Pedido(int _id){
+        this.id = _id;
+        recuperarDatos();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isEstadoPago() {
+        return estadoPago;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEstadoPago(boolean estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    
+    public void recuperarDatos() {
+        String sql = "select * from pedidos where dni = " + this.id;
+        Connection con = bd.Conexion();
+        if (con != null) {
+            // Informamos que la conexión es correcta
+            try {
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    this.fecha = rs.getString("fecha");
+                    this.formaPago = rs.getString("formaPago");
+                    this.estadoPago = Boolean.getBoolean("estadoPago");
+                    this.cliente = rs.getString("cliete");
+                }
+            } catch (Exception ex) {}
+        } else { // Sino informamos que no nos podemos conectar.
+            System.out.println("No hay conexion");
+        }
+    }
+    
+    public void registrar() {
+        String sql;
+        if (fecha == null){
+            sql = "insert into pedidos (formaPago, estadoPago, cliente) VALUES("+ this.formaPago + "," + this.estadoPago + "," + this.cliente + ")";
+        }
+        else{
+            sql = "insert into pedidos (fecha, formaPago, estadoPago, cliente) VALUES(" + this.fecha + "," + this.formaPago + "," + this.estadoPago + "," + this.cliente + ")";
+        }
+//        int nr = stmt.executeUpdate
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {
+            System.out.println("Error de Registro");
+        }   
+    }
+    
+    public void actualizar() {
+        //Actualizar la ficha del Cliente
+        String sql = "update pedidos set " + " fecha = '" + this.fecha +"', formaPago = '" + this.formaPago + "', estadoPago = '" + this.estadoPago + "',cliente = '" + this.cliente + "' where id = " + this.id;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+    
+    public void eliminar() {
+        String sql = "delete from pedidos where id = " + this.id;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+}
