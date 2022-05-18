91440e3f7d621ddf65ebd2ed5b2a2941326f6ce0
Vennuss <huchanserio@gmail.com>
2022-05-18 19:05:12 +0200

    Pedidos y .gitigonore añadidos

---
+++ src/Pedidos/ArticuloPedido.java
@@ -0,0 +1,107 @@
+package Pedidos;
+
+import bd.bd;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author admin
+ */
+public class ArticuloPedido {
+    final private int pedidoId;
+    private int articuloRef, cantidad;
+    private double precio;
+    
+    public ArticuloPedido(int _articulo, int _pedido, int _cantidad){
+        this.articuloRef = _articulo;
+        this.pedidoId = _pedido;
+        this.cantidad = _cantidad;
+    }
+    
+    public ArticuloPedido(int _articulo, int _pedido){
+        this.articuloRef = _articulo;
+        this.pedidoId = _pedido;
+        recuperarDatos();
+    }
+
+    public int getArticuloRef() {
+        return articuloRef;
+    }
+
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    public void recuperarDatos() {
+        String sql = "select * from contiene where articulosRef = " + this.articuloRef + " and pedidosId = " + this.pedidoId;
+        Connection con = bd.Conexion();
+        if (con != null) {
+            // Informamos que la conexión es correcta
+            try {
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    this.cantidad = rs.getInt("cantidad");
+                    this.precio = (double) rs.getLong("precio");
+                }
+            } catch (Exception ex) {}
+        } else { // Sino informamos que no nos podemos conectar.
+            System.out.println("No hay conexion");
+        }
+    }
+    
+    public void registrar() {
+        String sql = "insert into contiene VALUES(" + this.articuloRef + "," + this.pedidoId + "," + this.cantidad + "," + this.precio + ")";
+        
+//        int nr = stmt.executeUpdate
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {
+            System.out.println("Error de Registro");
+        }   
+    }
+    
+    public void actualizar() {
+        //Actualizar la ficha del Cliente
+        String sql = "update contiene set " + " articulosRef = '" + this.articuloRef +"', pedidosID = '" + this.pedidoId + "', cantidad = '" + this.cantidad + "',precio = '" + this.precio + "'"
+                + " where articulosRef = " + this.articuloRef + " and pedidosId = " + this.pedidoId;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+    
+    public void eliminar() {
+        String sql = "delete from contiene where articulosRef = " + this.articuloRef + " and pedidosId = " + this.pedidoId;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+    
+}
---
+++ src/Pedidos/Pedido.java
@@ -0,0 +1,142 @@
+package Pedidos;
+
+import bd.bd;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author admin
+ */
+public class Pedido {
+    
+    //final private String url = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private int id;
+    private boolean estadoPago;
+    private String fecha = null, formaPago, cliente;
+    
+    public Pedido(String _fecha, String _formaPago, boolean _estadoPago, String _cliente){
+        this.fecha = _fecha;
+        this.formaPago = _formaPago;
+        this.estadoPago = _estadoPago;
+        this.cliente = _cliente;
+    }
+    
+    public Pedido(String _formaPago, boolean _estadoPago, String _cliente){
+        this.formaPago = _formaPago;
+        this.estadoPago = _estadoPago;
+        this.cliente = _cliente;
+    }
+    
+    public Pedido(int _id){
+        this.id = _id;
+        recuperarDatos();
+    }
+
+    public Pedido() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isEstadoPago() {
+        return estadoPago;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEstadoPago(boolean estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    
+    public void recuperarDatos() {
+        String sql = "select * from pedidos where id = " + this.id;
+        Connection con = bd.Conexion();
+        if (con != null) {
+            // Informamos que la conexión es correcta
+            try {
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    this.fecha = rs.getString("fecha");
+                    this.formaPago = rs.getString("formaPago");
+                    this.estadoPago = Boolean.getBoolean("estadoPago");
+                    this.cliente = rs.getString("cliete");
+                }
+            } catch (Exception ex) {}
+        } else { // Sino informamos que no nos podemos conectar.
+            System.out.println("No hay conexion");
+        }
+    }
+    
+    public boolean registrar() {
+        String sql;
+        if (fecha == null){
+            sql = "insert into pedidos (formaPago, estadoPago, cliente) "
+                    + "VALUES('" + formaPago +"'," + estadoPago +",'" + cliente + "');";
+        }
+        else{
+            sql = "insert into pedidos (fecha, formaPago, estadoPago, cliente) "
+                    + "VALUES('" + fecha + "','"+ formaPago +"'," + estadoPago +",'" + cliente + "');";
+        }
+//        int nr = stmt.executeUpdate
+        int resultado=bd.Sentencia(sql);
+        if(resultado>0){
+            JOptionPane.showMessageDialog(null, "Registro Exitoso");
+            return true;
+        }
+        else return false;
+    }
+    
+    public void actualizar() {
+        //Actualizar la ficha del Cliente
+        String sql = "update pedidos set " + " fecha = '" + this.fecha +"', formaPago = '" + this.formaPago + "', estadoPago = '" + this.estadoPago + "',cliente = '" + this.cliente 
+                + "' where id = " + this.id;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+    
+    public void eliminar() {
+        String sql = "delete from pedidos where id = " + this.id;
+        Connection con = bd.Conexion();
+        try {
+            Statement st = con.createStatement();
+            int nr = st.executeUpdate(sql);
+            System.out.println("numero de registros actualizados:" + nr);
+        } catch (Exception ex) {}
+    }
+}
